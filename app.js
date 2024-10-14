64eefa26b3d8e519faad622d0fe8e82b91a76b47
Paisen <izansazziti3@gmail.com>
2024-10-14 17:18:26 +0800

    first working instance of aws lambda integration, changed app.js to index.js as serverless-http only detects index.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello! You found me!');
-});
-const port = 4000;
-app.listen(port, () => console.log(`Server is listening on port: ${port}`));
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const path = require('path');
+
+const app = express();
+//const port = 4000;
+
+app.use(express.json());
+app.use(express.static("profile-master"));
+
+app.get('/hello', (req, res) => {
+  res.sendFile(path.join(__dirname, '/profile-master/index.html'))
+});
+
+//app.listen(port, () => console.log(`Server is listening on port: ${port}`));
+
+module.exports.handler = serverless(app);
