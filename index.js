79a8c93f1226ab6e49f5c09f7a1f92a6634d3892
Paisen <izansazziti3@gmail.com>
2024-10-15 12:56:25 +0800

    Updated ui with latest information + touchups

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const serverless = require('serverless-http');
 const path = require('path');
 
 const app = express();
-//const port = 4000;
+const port = 4000;
 
 app.use(express.json());
 app.use(express.static("profile-master"));
@@ -12,6 +12,6 @@ app.get('/hello', (req, res) => {
   res.sendFile(path.join(__dirname, '/profile-master/index.html'))
 });
 
-//app.listen(port, () => console.log(`Server is listening on port: ${port}`));
+app.listen(port, () => console.log(`Server is listening on port: ${port}`));
 
 module.exports.handler = serverless(app);
